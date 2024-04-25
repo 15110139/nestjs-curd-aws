c6e22b86925aa894894226f93cbf77419be78e8c
alex.bui <tienbmdev97@gmail.com>
2024-04-26 00:54:39 +0700

    Rebuild

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'postgres',
       entities: [],
       synchronize: true,
+      ssl: false,
     }),
   ],
   controllers: [AppController],

c3e1d55ece040ef4d80cfaa8699d3be36d0a5fda
alex.bui <tienbmdev97@gmail.com>
2024-04-26 13:25:27 +0700

    Rebuild

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('main');
   logger.log('Start application on port 80');
-  await app.listen(80);
+  await app.listen(3000);
 }
 bootstrap();

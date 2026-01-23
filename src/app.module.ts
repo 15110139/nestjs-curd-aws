dbe00efcebadc5a90055359da5453ebc335b254a
alex.bui <alex.bui@fwd.com>
2026-01-24 01:16:34 +0700

    update pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health/health.controller';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TerminusModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'database-2.c5c9evc5kwlh.us-east-1.rds.amazonaws.com',
-    //   port: 5432,
-    //   username: 'postgres',
-    //   password: '12345678',
-    //   database: 'postgres',
-    //   entities: [],
-    //   synchronize: true,
-    //   ssl: false,
-    // }),
-  ],
+  imports: [TerminusModule, DatabaseModule, UsersModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
